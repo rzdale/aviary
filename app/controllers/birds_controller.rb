32cb219a1a35ee9c41db1dd5a82b2478e69ed1d5
Rob Dale <email": null>
2016-10-24 16:12:26 -0500

    Add show and create methods

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,26 @@
 class BirdsController < ApplicationController
   def index
-    birds = Bird.all
-    render json: birds
+    render json: Bird.all
   end
+
+  def show
+    render json: Bird.find(params[:id])
+  end
+
+  def create
+    newbird = Bird.new(
+                name: params[:name],
+                conservation_status: params[:conservation_status],
+                order: params[:order],
+                family: params[:family],
+                genus: params[:genus]
+              )
+
+    if newbird.save
+      render json: newbird
+    else
+      render json: newbird.errors
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get '/birds' => 'birds#index'
 
+  get '/birds/:id' => 'birds#show'
 
+  post 'birds' => 'birds#create'
 
 end
