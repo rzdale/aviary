e53d5081fbe8e995e9bcd05af53d75f7f9bac9c1
Rob Dale <email": null>
2016-11-10 09:58:08 -0600

    database yml, gitignore, root route

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
+
   get '/birds' => 'birds#index'
 
   get '/birds/:id' => 'birds#show'
 
   post 'birds' => 'birds#create'
+
+
+  root 'birds#index'
 
 end
