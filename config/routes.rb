55231d998bd7c70a6a154ec4fc01f8483362e43f
robzd1 <robzd1@gmail.com>
2017-01-20 18:12:54 +0000

    reformat routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+  root to: 'birds#index'
 
   get '/birds' => 'birds#index'
 
@@ -9,7 +9,5 @@ Rails.application.routes.draw do
 
   post 'birds' => 'birds#create'
 
-
-  root 'birds#index'
 
 end
